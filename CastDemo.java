daa2b4e483f3f980f74c20db5008be45bca11167
ik1ant <124435987+ik1ant@users.noreply.github.com>
2024-05-14 00:13:47 +0300

    Add files via upload

---
+++ CastDemo.java
@@ -0,0 +1,11 @@
+
+public class CastDemo {
+    public static void main(String[] args) {
+        double x, y;
+        x = 5.0;
+        y = 2.5;
+
+        var result = (int) (x / y);
+        System.out.println(result);
+    }
+}
